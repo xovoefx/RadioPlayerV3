9dbbcf829dab1165a6dc60b0306969a314520c0d
xovoefx <98139422+xovoefx@users.noreply.github.com>
2022-08-06 10:01:05 +0530

    Update inline.py

--- plugins/bot/inline.py
+++ plugins/bot/inline.py
@@ -31,11 +31,11 @@ buttons = [
                 InlineKeyboardButton("❔ HOW TO USE ME ❔", callback_data="help"),
             ],
             [
-                InlineKeyboardButton("CHANNEL", url="https://t.me/AsmSafone"),
-                InlineKeyboardButton("SUPPORT", url="https://t.me/AsmSupport"),
+                InlineKeyboardButton("CHANNEL", url="https://t.me/mmmoviesofficial"),
+                InlineKeyboardButton("SUPPORT", url="https://t.me/musicxsupportmm"),
             ],
             [
-                InlineKeyboardButton("🤖 MAKE YOUR OWN BOT 🤖", url="https://heroku.com/deploy?template=https://github.com/AsmSafone/RadioPlayerV3"),
+                InlineKeyboardButton("🤖 MAKE YOUR OWN BOT 🤖", url="https://t.me/musicxsupport"),
             ]
          ]
 
@@ -49,7 +49,7 @@ async def search(client, query):
                 title="Deploy Your Own Radio Player",
                 thumb_url="https://telegra.ph/file/4e839766d45935998e9c6.jpg",
                 photo_url="https://telegra.ph/file/4e839766d45935998e9c6.jpg",
-                caption=f"{REPLY_MESSAGE}\n\n<b>© Powered By : \n@AsmSafone | @AsmSupport 👑</b>",
+                caption=f"{REPLY_MESSAGE}\n\n<b>© Powered By : \n@xovo_efx | @xovo_efx 👑</b>",
                 reply_markup=InlineKeyboardMarkup(buttons)
                 )
             )
